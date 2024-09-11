6ee5b995a8a239bdd55c7873ad58048eea080d7a
u120022 <u120022@st.pu-toyama.ac.jp>
2024-09-11 12:53:53 +0900

    left-turned car curvature history

--- simulation/src/main.rs
+++ simulation/src/main.rs
@@ -11,75 +11,96 @@ fn main() -> eframe::Result {
     )
 }
 
+#[derive(Default)]
 struct App {
     left_turned_var: LeftTurnedVar,
     left_turned_data: Option<LeftTurnedData>,
 }
 
-impl Default for App {
-    fn default() -> Self {
-        Self {
-            left_turned_var: Default::default(),
-            left_turned_data: Default::default(),
-        }
-    }
-}
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Sumulation");
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.heading("Variable");
 
-            let widget = egui::Slider::new(&mut self.left_turned_var.v_in, 0.0..=100.0)
-                .text("inflow velocity[m/sec]");
-            ui.add(widget);
+                let widget = egui::Slider::new(&mut self.left_turned_var.v_in, 0.0..=100.0)
+                    .text("inflow velocity[m/sec]");
+                ui.add(widget);
 
-            let widget = egui::Slider::new(&mut self.left_turned_var.v_out, 0.0..=100.0)
-                .text("outflow velocity[m/sec]");
-            ui.add(widget);
+                let widget = egui::Slider::new(&mut self.left_turned_var.v_out, 0.0..=100.0)
+                    .text("outflow velocity[m/sec]");
+                ui.add(widget);
 
-            let widget = egui::Slider::new(&mut self.left_turned_var.angle, 0.0..=180.0)
-                .text("intersection angle[deg]");
-            ui.add(widget);
+                let widget = egui::Slider::new(&mut self.left_turned_var.angle, 0.0..=180.0)
+                    .text("intersection angle[deg]");
+                ui.add(widget);
 
-            let widget = egui::Slider::new(&mut self.left_turned_var.radius, 0.0..=30.0)
-                .text("border radius[m]");
-            ui.add(widget);
+                let widget = egui::Slider::new(&mut self.left_turned_var.radius, 0.0..=30.0)
+                    .text("border radius[m]");
+                ui.add(widget);
 
-            let widget = egui::Slider::new(&mut self.left_turned_var.padding, 0.0..=10.0)
-                .text("distance from border[m]");
-            ui.add(widget);
+                let widget = egui::Slider::new(&mut self.left_turned_var.padding, 0.0..=10.0)
+                    .text("distance from border[m]");
+                ui.add(widget);
 
-            let widget = egui::Checkbox::new(&mut self.left_turned_var.large, "large car type");
-            ui.add(widget);
+                let widget = egui::Checkbox::new(&mut self.left_turned_var.large, "large car type");
+                ui.add(widget);
 
-            if ui.button("Compute").clicked() {
-                let left_turned_distr = LeftTurnedDistr::new(&self.left_turned_var);
-                let left_turned_data = LeftTurnedData::sample(
-                    &mut rand::thread_rng(),
-                    &self.left_turned_var,
-                    &left_turned_distr,
-                );
-                self.left_turned_data = Some(left_turned_data);
-            }
+                if ui.button("Compute").clicked() {
+                    let left_turned_distr = LeftTurnedDistr::new(&self.left_turned_var);
+                    let left_turned_data = LeftTurnedData::sample(
+                        &mut rand::thread_rng(),
+                        &self.left_turned_var,
+                        &left_turned_distr,
+                    );
+                    self.left_turned_data = Some(left_turned_data);
+                }
 
-            // plot
-            if let Some(data) = &self.left_turned_data {
-                ui.label(format!(
-                    "[Param] c_in: {:.2}, c_out: {:.2}, v_min: {:.2}, x_min: {:.2}, t_min: {:.2}, t_exit: {:.2}",
-                    data.c_in, data.c_out, data.v_min, data.x_min, data.t_min, data.t_exit
-                ));
+                // plot
+                if let Some(data) = &self.left_turned_data {
+                    ui.heading("Result");
 
-                let velocity_line = egui_plot::Line::new(data.velocity_series.clone());
-                egui_plot::Plot::new("Velocity-Time Plot")
-                    .view_aspect(2.0)
-                    .show(ui, |plot_ui| plot_ui.line(velocity_line));
+                    ui.label(format!("c_in: {:.4}", data.c_in));
+                    ui.label(format!("c_out: {:.4}", data.c_out));
+                    ui.label(format!("v_min: {:.4}", data.v_min));
+                    ui.label(format!("x_min: {:.4}", data.x_min));
+                    ui.label(format!("t_min: {:.4}", data.t_min));
+                    ui.label(format!("t_exit: {:.4}", data.t_exit));
+                    ui.label(format!("t_o: {:.4}", data.t_o));
+                    ui.label(format!("x_o: {:.4}", data.x_o));
 
-                let position_line = egui_plot::Line::new(data.position_series.clone());
-                egui_plot::Plot::new("Position-Time Plot")
-                    .view_aspect(2.0)
-                    .show(ui, |plot_ui| plot_ui.line(position_line));
-            }
+                    ui.separator();
+
+                    ui.label("Velocity-Time Plot");
+                    egui_plot::Plot::new("Velocity-Time Plot")
+                        .view_aspect(2.0)
+                        .allow_scroll(false)
+                        .show(ui, |plot_ui| {
+                            plot_ui.line(egui_plot::Line::new(data.velocity_series.clone()));
+                            plot_ui.vline(egui_plot::VLine::new(data.t_o));
+                        });
+
+                    ui.label("Position-Time Plot");
+                    egui_plot::Plot::new("Position-Time Plot")
+                        .view_aspect(2.0)
+                        .allow_scroll(false)
+                        .show(ui, |plot_ui| {
+                            plot_ui.line(egui_plot::Line::new(data.position_series.clone()));
+                            plot_ui.vline(egui_plot::VLine::new(data.t_o));
+                            plot_ui.hline(egui_plot::HLine::new(data.x_o));
+                        });
+
+                    ui.label("Curvature-Position Plot");
+                    egui_plot::Plot::new("Curvature-Position Plot")
+                        .view_aspect(2.0)
+                        .allow_scroll(false)
+                        .show(ui, |plot_ui| {
+                            plot_ui.line(egui_plot::Line::new(data.curvature_series.clone()));
+                            plot_ui.vline(egui_plot::VLine::new(data.x_o));
+                        });
+                }
+            });
         });
     }
 }
@@ -113,6 +134,7 @@ struct LeftTurnedDistr {
     c_out: rand_distr::Gamma<f64>,
     v_min: rand_distr::Normal<f64>,
     x_min: rand_distr::Normal<f64>,
+    r_min: rand_distr::Normal<f64>,
 }
 
 impl LeftTurnedDistr {
@@ -167,11 +189,21 @@ impl LeftTurnedDistr {
         let std_dev = b.dot(&y).max(f64::EPSILON);
         let x_min = rand_distr::Normal::new(mean, std_dev).unwrap();
 
+        // r_min parameter
+        let a = nalgebra::vector![0.127, 0.390, 0.862, -6.46];
+        let x = nalgebra::vector![var.angle, var.radius, var.padding, 1.0,];
+        let mean = a.dot(&x);
+        let b = nalgebra::vector![0.0363, 0.0624, 0.118, -2.86];
+        let y = nalgebra::vector![var.angle, var.radius, var.padding, 1.0,];
+        let std_dev = b.dot(&y).max(f64::EPSILON);
+        let r_min = rand_distr::Normal::new(mean, std_dev).unwrap();
+
         Self {
             c_in,
             c_out,
             v_min,
             x_min,
+            r_min,
         }
     }
 }
@@ -184,8 +216,11 @@ struct LeftTurnedData {
     x_min: f64,
     t_min: f64,
     t_exit: f64,
+    t_o: f64,
+    x_o: f64,
     velocity_series: Vec<[f64; 2]>,
     position_series: Vec<[f64; 2]>,
+    curvature_series: Vec<[f64; 2]>,
 }
 
 impl LeftTurnedData {
@@ -196,8 +231,12 @@ impl LeftTurnedData {
         let c_in = rand::Rng::sample(rng, distr.c_in);
         let c_out = rand::Rng::sample(rng, distr.c_out);
         let v_min = rand::Rng::sample(rng, distr.v_min);
+        let x_min = rand::Rng::sample(rng, distr.x_min);
+        let r_min = rand::Rng::sample(rng, distr.r_min);
 
         let mut velocity_series = vec![];
+        let mut position_series = vec![];
+        let mut curvature_series = vec![];
 
         // inflow
         let t_min = (2.0 / c_in * (var.v_in - v_min)).cbrt();
@@ -234,7 +273,6 @@ impl LeftTurnedData {
         // position
         let mut x = 0.0;
         let mut x_min_ = 0.0;
-        let mut position_series = vec![];
         for w in velocity_series.windows(2) {
             let [t0, v0] = w[0];
             let [t1, v1] = w[1];
@@ -246,10 +284,52 @@ impl LeftTurnedData {
             x += (t1 - t0) * (v0 + v1) * 0.5;
             position_series.push([t1, x]);
         }
-
-        // shift
-        let x_min = rand::Rng::sample(rng, distr.x_min);
         let x_o = x_min_ - x_min;
+
+        // curvature
+        let a = nalgebra::vector![-1.65, 0.0404, 0.334, 0.0, 0.461, 0.369];
+        let x = nalgebra::vector![
+            1.0,
+            var.angle,
+            var.radius,
+            if var.large { 1.0 } else { 0.0 },
+            var.padding,
+            v_min
+        ];
+        let a1 = a.dot(&x);
+        let a = nalgebra::vector![2.33, 0.0, 0.335, 2.05, 1.04, 0.268];
+        let x = nalgebra::vector![
+            1.0,
+            var.angle,
+            var.radius,
+            if var.large { 1.0 } else { 0.0 },
+            var.padding,
+            v_min
+        ];
+        let a2 = a.dot(&x);
+        let l_clothoid1 = r_min.recip() / a1.powi(2).recip();
+        let angle_clothoid1 = 0.5 * a1.powi(2).recip() * l_clothoid1.powi(2);
+        let l_clothoid2 = r_min.recip() / a2.powi(2).recip();
+        let angle_clothoid2 = 0.5 * a2.powi(2).recip() * l_clothoid2.powi(2);
+        let angle_arc = var.angle.to_radians() - (angle_clothoid1 + angle_clothoid2);
+        let l_arc = angle_arc / r_min.recip();
+        let x_0 = x_o;
+        let x_1 = x_0 + l_clothoid1;
+        let x_2 = x_1 + l_arc;
+        let x_3 = x_2 + l_clothoid2;
+        for &[_, x] in &position_series {
+            if x_0 <= x && x < x_1 {
+                curvature_series.push([x, a1.powi(2).recip() * (x - x_0)]);
+            } else if x_1 <= x && x < x_2 {
+                curvature_series.push([x, r_min.recip()]);
+            } else if x_2 <= x && x < x_3 {
+                curvature_series.push([x, r_min.recip() - a2.powi(2).recip() * (x - x_2)]);
+            } else {
+                curvature_series.push([x, 0.0]);
+            }
+        }
+
+        // origin time
         let mut t_o = 0.0;
         for w in position_series.windows(2) {
             let [t, x0] = w[0];
@@ -259,13 +339,18 @@ impl LeftTurnedData {
                 t_o = t;
             }
         }
-        for [t, _] in velocity_series.iter_mut() {
-            *t -= t_o;
-        }
-        for [t, x] in position_series.iter_mut() {
-            *t -= t_o;
-            *x -= x_o;
-        }
+
+        // // origin shift
+        // for [t, _] in velocity_series.iter_mut() {
+        //     *t -= t_o;
+        // }
+        // for [t, x] in position_series.iter_mut() {
+        //     *t -= t_o;
+        //     *x -= x_o;
+        // }
+        // for [x, _] in curvature_series.iter_mut() {
+        //     *x -= x_o;
+        // }
 
         Self {
             c_in,
@@ -274,8 +359,11 @@ impl LeftTurnedData {
             x_min,
             t_min,
             t_exit: t_min + t_next,
+            t_o,
+            x_o,
             velocity_series,
             position_series,
+            curvature_series,
         }
     }
 }
